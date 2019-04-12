e4f664616e327f9dafaf7447b39399aa2d7ff72d
chihiro-y <chiro1997@gmail.com>
2019-04-12 17:42:22 +0900

    選択を配列に保持できるように変更

--- src/Question/QuestionCard.js
+++ src/Question/QuestionCard.js
@@ -4,27 +4,43 @@ import Button from './Button';
 
 class QuestionCard extends Component {
     constructor(props){
-        super(props);
-		this.state = {
-			question_num : 0,
-			question_all : 4,
-			questionAnswer : []
-		};
+      super(props);
+			this.state = {
+				question_num : 0,
+				question_all : 4,
+				question_answer : []
+			};
     }
     render() {
-		return (
-		<div>
-			<div>{this.state.question_num + 1 } / {this.state.question_all + 1}</div>
-			<QuestionSentence question_num = {this.state.question_num} />
-			<Button id = 'YesButton' question_num = {() => {this.onYesnoButtonClick();}} />
-			<Button id = 'NoButton' question_num = {() => {this.onYesnoButtonClick();}} />
-		</div>
-		);
-	}
+			return (
+				<div>
+					<div>{this.state.question_num + 1 } / {this.state.question_all + 1}</div>
+					<QuestionSentence className = 'QuestionSentence' question_num = {this.state.question_num} />
+					<Button className = 'YesButton' question_num = {() => {this.onYesButtonClick();}} />
+					<Button className = 'NoButton' question_num = {() => {this.onNoButtonClick();}} />
+					{this.state.question_answer}
+				</div>
+			);
+		}
     	  
-	onYesnoButtonClick = () => {
-		this.setState({ question_num: this.state.question_num + 1});
-	}
+		onYesButtonClick = () => {
+			this.setState(
+				{
+					question_num: this.state.question_num + 1, 
+					question_answer : this.state.question_answer.concat('0')
+				}
+			);
+
+		}
+		onNoButtonClick = () => {
+			this.setState(
+				{
+					question_num: this.state.question_num + 1,
+					question_answer : this.state.question_answer.concat('1')
+				}
+			);
+
+		}
 }
 
 export default QuestionCard;
